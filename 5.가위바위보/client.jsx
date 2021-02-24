f05c8560edcbf52fd62ee821e5a4854968d0cc7f
6mn12j <dbwk0404@hanmail.net>
2021-02-25 04:40:00 +0900

    Refactor: 가위바위보 Hooks

--- 5.가위바위보/client.jsx
+++ 5.가위바위보/client.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import RSP from './RSP_hooks';
+import RSP from './RSP';
 
 ReactDOM.render(<RSP />, document.querySelector('#root'));
