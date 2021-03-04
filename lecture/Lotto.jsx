93b9fe9a59073f65838ea083065df6339c7c877d
6mn12j <dbwk0404@hanmail.net>
2021-03-04 20:19:19 +0900

    Feat: 로또 공 뽑기

--- lecture/Lotto.jsx
+++ lecture/Lotto.jsx
@@ -6,9 +6,9 @@ function getWinNumbers() {
   const candidate = Array(45)
     .fill()
     .map((v, i) => i + 1);
-  const fhuffle = [];
+  const shuffle = [];
   while (candidate.length > 0) {
-    shuffle.push(candidate.splice(Math, floor(Math.random() * dandidate.length), [0]));
+    shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
   }
   const bonusNumber = shuffle[shuffle.length - 1];
   const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
@@ -22,6 +22,30 @@ class Lotto extends Component {
     bonus: null, // 보너스 공
     redo: false,
   };
+
+  timeouts = [];
+  componentDidMount() {
+    const { winNumbers } = this.state;
+    for (let i = 0; i < winNumbers.length - 1; i++) {
+      this.timeouts[i] = setTimeout(() => {
+        this.setState((prevState) => {
+          return { winBalls: [...prevState.winBalls, winNumbers[i]] };
+        });
+      }, (i + 1) * 1000);
+    }
+    this.timeouts[6] = setTimeout(() => {
+      this.setState({
+        bonus: winNumbers[6],
+        redo: true,
+      });
+    }, 7000);
+  }
+
+  componentWillUnmount() {
+    this.timeouts.forEach((v) => {
+      clearTimeout(v);
+    });
+  }
   render() {
     const { winBalls, bonus, redo } = this.state;
     return (
